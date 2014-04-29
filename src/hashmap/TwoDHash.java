7de1bad951b5383f91eda38f486a4205fa7c489a
Lu Shuai <sl988@cornell.edu>
2014-04-28 23:42:28 -0400

    start building the 2D hash

---
+++ src/hashmap/TwoDHash.java
@@ -0,0 +1,15 @@
+package hashmap;
+
+public class TwoDHash<K, V>
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/someTest/ShortestPath.java
@@ -0,0 +1,89 @@
+package someTest;
+
+import java.util.HashSet;
+
+public class ShortestPath
+{
+	public class node
+	{
+		int x;
+		int y;
+		public node(int a, int b)
+		{
+			x = a;
+			y = b;
+		}
+	}
+
+	
+	 public int shortestPath(int[][] map)
+	 {
+		if(map == null || map.length == 0 || map[0].length == 0)
+			return -1;
+		int[] min = new int[1];
+		min[0] = Integer.MAX_VALUE;
+		shortestPathHelper(map, new node(0,0), 0, new HashSet<node>(), min);
+		return min[0];
+	 }
+	 
+	 private void shortestPathHelper(int[][] map, node cNode, int cCost, HashSet<node> path, int[] min)
+	 {
+		 System.out.println("checking node : " + cNode.x  + ", " + cNode.y);
+		 if(cNode.x == map.length && cNode.y == map.length)
+		 {
+			 min[0] = cCost < min[0] ? cCost : min[0];
+			 return;
+		 }
+		 //get rid of intersections
+		 if(path.contains(cNode))
+		 {
+			 return;
+		 }
+		 HashSet<node> pathSoFar = new HashSet<node>(path);
+		 pathSoFar.add(cNode);
+		 //search right
+		 if(cNode.x < map.length-1)
+		 {
+			 shortestPathHelper(map, new node(cNode.x + 1, cNode.y), cCost + map[cNode.x][cNode.y], pathSoFar, min);
+		 }
+		 //search left
+		 if(cNode.x > 0)
+		 {
+			 shortestPathHelper(map, new node(cNode.x -1, cNode.y), cCost + map[cNode.x][cNode.y], pathSoFar, min);
+		 }
+		 //search bottom
+		 if(cNode.y < map.length-1)
+		 {
+			 shortestPathHelper(map, new node(cNode.x, cNode.y + 1), cCost + map[cNode.x][cNode.y], pathSoFar, min);
+		 }
+		 //search top
+		 if(cNode.y > 0)
+		 {
+			 shortestPathHelper(map, new node(cNode.x, cNode.y-1), cCost + map[cNode.x][cNode.y], pathSoFar, min);
+		 }
+	 }
+	
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		/*
+		int[][] a = {
+				{0, 1, 2},
+				{1, 2, 3},
+				{2, 2, 2}
+		};
+		ShortestPath sp = new ShortestPath();
+		System.out.println(sp.shortestPath(a));
+		*/
+		
+		HashSet<ShortestPath.node> set = new HashSet<ShortestPath.node>();
+		ShortestPath sp = new ShortestPath();
+		set.add(sp.new node(1,2));
+		ShortestPath.node node2 = sp.new node(1,2);
+		System.out.println(set.contains(node2));
+	}
+
+}
