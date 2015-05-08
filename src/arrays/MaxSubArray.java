9c8f35431897a3fa1f72557107ddd3b11f715c78
Shuai <shuailu@yahoo-inc.com>
2015-05-08 16:11:52 -0700

    surrogate test and max sub array prac

--- src/arrays/MaxSubArray.java
+++ src/arrays/MaxSubArray.java
@@ -50,8 +50,73 @@ public class MaxSubArray
 	{
 		//int[] a = {-5,-1,1,2,-10,2,-1,3};
 		int[] a = {1,2,3,4,-100,10,-1,2};
-		System.out.println("start: "+maxSub(a)[0]);
-		System.out.println("length: "+maxSub(a)[1]);
+		System.out.println("start: "+maxSub2(a)[0]);
+		System.out.println("length: "+maxSub2(a)[1]);
 	}
+
+
+
+
+   // range:   [start, end)
+
+    public static int[] maxSub2(int[] a) {
+        int currentSum = 0;
+        int currentStart = -1;
+        int currentEnd = 0;
+
+        int max = Integer.MIN_VALUE;
+        int maxStart = 0;
+        int maxEnd = 0;
+
+        for (int n = 0; n < a.length; n++) {
+            if (currentStart < 0) {
+                currentStart = n;
+                currentEnd = currentStart;
+            }
+            currentSum += a[n];
+            currentEnd ++ ;
+            if (currentSum > max) {
+                max = currentSum;
+                maxStart = currentStart;
+                maxEnd = currentEnd;
+            }
+
+            if (currentSum < 0) {
+                currentStart = -1;
+                currentSum = 0;
+            }
+        }
+
+        int[] res = new int[2];
+        res[0] = maxStart;
+        res[1] = maxEnd - 1;
+        return res;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
---
+++ src/someTest/SurrodateTest.java
@@ -0,0 +1,27 @@
+package someTest;
+
+/**
+ * Created by shuailu on 5/8/15.
+ */
+public class SurrodateTest {
+
+   public static void main(String[] args) {
+       //int codepoint = 0x24B62;
+       int codepoint = 0x1F484;
+       if (((codepoint >= 0x20) && (codepoint <= 0xD7FF)) ||
+           ((codepoint >= 0xE000) && (codepoint <= 0xFFFD)) ||
+           ((codepoint >= 0x10000) && (codepoint <= 0x10FFFF))) {
+           System.out.println("legal");
+       }
+
+       String str = new String(Character.toChars(codepoint));
+       for (int n = 0; n < str.length(); n++) {
+           if (Character.isSurrogate(str.charAt(n))) {
+               System.out.println("is surrogate");
+           }
+       }
+
+   }
+
+
+}
