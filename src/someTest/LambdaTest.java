618ae804784b3375439676b4d5c528ea54b23ddb
Shuai Lu <sulu@sulu-mn1.linkedin.biz>
2016-10-13 17:43:49 -0700

    lambda demo

---
+++ src/someTest/LambdaTest.java
@@ -0,0 +1,50 @@
+package someTest;
+
+/**
+ * Created by Shuai Lu on 10/13/16.
+ */
+public class LambdaTest {
+    public static void main(String args[]){
+        LambdaTest tester = new LambdaTest();
+
+        //with type declaration
+        MathOperation addition = (int a, int b) -> a + b;
+
+        //with out type declaration
+        MathOperation subtraction = (a, b) -> a - b;
+
+        //with return statement along with curly braces
+        MathOperation multiplication = (int a, int b) -> { return a * b; };
+
+        //without return statement and without curly braces
+        MathOperation division = (int a, int b) -> a / b;
+
+        System.out.println("10 + 5 = " + tester.operate(10, 5, addition));
+        System.out.println("10 - 5 = " + tester.operate(10, 5, subtraction));
+        System.out.println("10 x 5 = " + tester.operate(10, 5, multiplication));
+        System.out.println("10 / 5 = " + tester.operate(10, 5, division));
+
+        //with parenthesis
+        GreetingService greetService1 = message ->
+                System.out.println("Hello " + message);
+
+        //without parenthesis
+        GreetingService greetService2 = (message) ->
+                System.out.println("Hello " + message);
+
+        greetService1.sayMessage("Mahesh");
+        greetService2.sayMessage("Suresh");
+    }
+
+    interface MathOperation {
+        int operation(int a, int b);
+    }
+
+    interface GreetingService {
+        void sayMessage(String message);
+    }
+
+    private int operate(int a, int b, MathOperation mathOperation){
+        return mathOperation.operation(a, b);
+    }
+}
